d25dbe22363a0209ae81d11634f8acb9b2f96fde
Wiozie <92623738+Wiozie@users.noreply.github.com>
2023-03-18 16:10:25 +0100

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,44 @@
+//Napisz program, który umożliwi sortowanie bąbelkowe wartości wprowadzonych przez
+//użytkownika
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj ilość liczb do posortowania: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Podaj liczby do posortowania:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("Posortowane liczby:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
