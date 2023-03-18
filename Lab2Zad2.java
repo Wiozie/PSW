c43876692a9b467fd07e63a81b5a2a384921a20f
Wiozie <92623738+Wiozie@users.noreply.github.com>
2023-03-18 16:32:35 +0100

    Update Lab2Zad2.java

--- Lab2Zad2.java
+++ Lab2Zad2.java
@@ -11,7 +11,7 @@
 // import java.util.Arrays;
 // import java.util.Scanner;
 
-public class ArrayStatistics {
+public class Lab2Zad2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
