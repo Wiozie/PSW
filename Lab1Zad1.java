d0de281a9f1dd44a567891e5471b4240fce4b75a
Wiozie <92623738+Wiozie@users.noreply.github.com>
2023-03-18 15:23:47 +0100

    Update and rename LAB1_ZAD1.java to Lab1Zad1.java

--- Lab1Zad1.java
+++ Lab1Zad1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class LAB1_ZAD1 {
+public class Lab1Zad1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
